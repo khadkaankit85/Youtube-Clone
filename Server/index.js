bce6cac000cf9f3137351e1e0cabf48aecccd84d
Angkit Khadka <khadkaankit85@gmail.com>
2024-09-16 23:41:41 -0400

    channel videos route created

--- Server/index.js
+++ Server/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 // backend.js (Node.js + Express)
 import express from 'express';
 import dotenv from 'dotenv';
@@ -7,12 +8,36 @@ import axios from "axios"
 dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Use the environment variable (API key)
-const baseUrl = "https://youtube-v31.p.rapidapi.com";
 // eslint-disable-next-line no-undef
 const apiKey = process.env.NOT_YOUTUBE_API_KEY
+
+//route to get videos of a channel videos
+app.get("/channel/videos", async (req, res) => {
+
+    const channelID = req.query.channelID
+    const options = {
+        method: 'GET',
+        url: 'https://yt-api.p.rapidapi.com/channel/videos',
+        params: {
+            id: channelID
+        },
+        headers: {
+            'x-rapidapi-key': apiKey,
+            'x-rapidapi-host': 'yt-api.p.rapidapi.com'
+        }
+    };
+
+    try {
+        const response = await axios.request(options);
+        res.send(response)
+    } catch (error) {
+        console.error(error);
+        res.status(304).send("server error")
+    }
+})
 
 
 app.listen(port, () => {
