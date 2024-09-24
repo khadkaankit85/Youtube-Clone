b013d452504b3faba272b8941dbd20e914a7004b
Angkit Khadka <khadkaankit85@gmail.com>
2024-09-23 20:09:18 -0400

    dubgging

--- Server/functions/api.js
+++ Server/functions/api.js
@@ -1,13 +1,16 @@
 import express from 'express';
 import serverless from 'serverless-http';
-import router from "../../Src/index.js";
+import router from "../Src/index.js";
 
 const app = express();
-app.get('/', (req, res) => {
-    res.send("hello world")
-});
-app.use('/.netlify/functions/api', router);  // path must route to lambda
 
+// Simple health check route
+app.get('/', (req, res) => {
+    res.send("hello world");
+});
+
+// Use the imported router for API routes
+app.use('/.netlify/functions/api', router);  // path must route to lambda
 
 export default app;
 export const handler = serverless(app);
